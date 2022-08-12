394f7e4de49d35afe4c5f09e65729d8ff04530b8
Sathya <sathyamelwani@gmail.com>
2022-08-12 15:35:59 +0800

    updated rest controller

--- sathyassfassessment/src/main/controllers/NewsRestController.java
+++ sathyassfassessment/src/main/controllers/NewsRestController.java
@@ -1,7 +1,15 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
+@RestController
 public class NewsRestController {
-    
 
+    @GetMapping(value = "/articleslist", consumes ="application/json", produces = "application/json")
+    public GetArticles createGetArticles(@RequestBody GetArticles getarticles) {
+
+
+        return getarticles;
+
+
+}
 }
