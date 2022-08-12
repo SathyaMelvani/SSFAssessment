4e4503fe41c5825efc6c1aa702d91d4ccb351755
Sathya <sathyamelwani@gmail.com>
2022-08-12 09:23:40 +0800

    generated a new SpringBoot application

---
+++ sathyassfassessment/src/main/java/nusiss/vttp/sathyassfassessment/SathyassfassessmentApplication.java
@@ -0,0 +1,13 @@
+package nusiss.vttp.sathyassfassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SathyassfassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SathyassfassessmentApplication.class, args);
+	}
+
+}
---
+++ sathyassfassessment/src/test/java/nusiss/vttp/sathyassfassessment/SathyassfassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package nusiss.vttp.sathyassfassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SathyassfassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
